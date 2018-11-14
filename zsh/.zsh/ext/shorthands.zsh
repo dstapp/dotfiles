75392e063842d55f3227935f647ba9b422e1251f
David Prandzioch <hello+git@davd.eu>
2018-11-14 10:15:24 +0100

    Updated shorthands

--- zsh/.zsh/ext/shorthands.zsh
+++ zsh/.zsh/ext/shorthands.zsh
@@ -1,8 +1,4 @@
 # Shorthands
 alias ws='cd ~/Workspace/ && ls'
 alias reload='source ~/.zshrc'
-alias jsdev='docker run -it --rm -v $(pwd):/src -w /src -p 4200:4200 davd/jsdev'
 alias dc='docker-compose'
-alias dm='docker-machine'
-alias reload-docker='docker-machine env default'
-alias wpscan='docker run -it --rm wpscanteam/wpscan'
